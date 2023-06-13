ebbee6166fdbf7933f95f4b7f25467fe62db6d38
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-13 11:47:45 -0400

    spelling: surrogate
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fmtutils.c
+++ fmtutils.c
@@ -154,7 +154,7 @@ utf_16le_to_utf_8_truncated(const wchar_t *utf_16, char *utf_8,
 					uc = SURROGATE_VALUE(c, c2);
 				} else {
 					/*
-					 * Not a trailing surroage;
+					 * Not a trailing surrogate;
 					 * try to drop in a
 					 * REPLACEMENT CHARACTER.
 					 */
